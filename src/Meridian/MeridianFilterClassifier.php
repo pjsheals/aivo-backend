3240b22dbabc893dd606865f83719ec9351107f2
pjsheals <166728879+pjsheals@users.noreply.github.com>
2026-04-15 23:08:03 +0100

    Change UUID generation method in MeridianFilterClassifier

--- src/Meridian/MeridianFilterClassifier.php
+++ src/Meridian/MeridianFilterClassifier.php
@@ -335,7 +335,7 @@ MSG;
         array  $parsed,
         array  $rawOutput
     ): string {
-        $id = \Illuminate\Support\Str::uuid()->toString();
+        $id = sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x', mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0x0fff) | 0x4000, mt_rand(0, 0x3fff) | 0x8000, mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff));
 
         DB::table('meridian_filter_classifications')->insert([
             'id'                     => $id,
